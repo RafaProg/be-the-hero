a6293a825d1a0cfd2a445f915f08479b1da9fcdb
Rafael <herminathor@gmail.com>
2020-03-28 11:22:28 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const crypto = require('crypto');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const connection = require('./database/connection');
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+routes.post('/session', SessionController.create);
+
+routes.get('/incident', IncidentController.index);
+routes.post('/incident', IncidentController.create);
+routes.delete('/incident/:id', IncidentController.delete);
+
+module.exports = routes;
